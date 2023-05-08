24fe2692c4e9d17f707e4c2d2702e7058dd1f607
Levitskyy <iv.levickiy@gmail.com>
2023-05-08 16:00:21 +0300

    added jump

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,9 +6,13 @@ using UnityEngine;
 public class PlayerController : NetworkBehaviour
 {
     [SerializeField] private float moveSpeed;
+    [SerializeField] private float groundCheckDistance;
+    [SerializeField] private float jumpForce;
+    [SerializeField] private LayerMask groundMask;
     private Rigidbody2D rb;
     private CapsuleCollider2D col;
     private float moveVector;
+    public bool isGrounded = false;
 
     void Start() {
         rb = GetComponent<Rigidbody2D>();
@@ -16,7 +20,12 @@ public class PlayerController : NetworkBehaviour
     }
 
     void Update() {
-
+        if (Physics2D.Raycast(new Vector2(transform.position.x, transform.position.y), Vector2.down, groundCheckDistance, groundMask)) {
+            isGrounded = true;
+        }
+        else {
+            isGrounded = false;
+        }
         HandlePlayerMovement();
     }
 
@@ -26,5 +35,8 @@ public class PlayerController : NetworkBehaviour
 
     private void HandlePlayerMovement() {
         moveVector = Input.GetAxisRaw("Horizontal");
+        if (isGrounded && Input.GetKeyDown(KeyCode.Space)) {
+            rb.AddForce(Vector2.up * jumpForce);
+        }
     }
 }
